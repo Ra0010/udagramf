c179399f83b1a6a17966f26a3c0929d469396d18
Ahmed H <91718675+Ra0010@users.noreply.github.com>
2022-09-01 23:17:31 +0100

    Add files via upload

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=d11
+export POSTGRES_PASSWORD=rdsdbpassword
+export POSTGRES_HOST=d11.cuw3zbg8sdkn.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=d11
+export AWS_BUCKET=arn:aws:s3:::udagram000
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:5432
